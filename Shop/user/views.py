2a97d6b3ec8bc8e96a3f3d88c395bb516e96fb2b
Fatemeh <msbabaei7@gmail.com>
2025-03-23 13:06:58 +0330

    add hello_world.py file change user

---
+++ Shop/user/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        pass
+    pass
+
+def login(request):
+    if request.method == 'POST':
+        pass
+    pass
+
